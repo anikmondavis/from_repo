ae7c1ba913a923e097dec82eec362e0384170ae9
anikmondavis <95902241+anikmondavis@users.noreply.github.com>
2022-12-20 12:09:10 +0530

    Update sample_email.py

--- sample_email.py
+++ sample_email.py
@@ -1,3 +1,48 @@
+EMAIL_ADDRESS='anikdavis18@gmail.com'
+EMAIL_PASSWORD='dlsaanik18'
+'''
+with smtplib.SMTP('smtp.gmail.com',587) as eml:
+    eml.ehlo()
+    eml.starttls()
+    eml.smtp.ehlo()
+
+    eml.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
+
+    subject='test image'
+    body='how about test'
+    msg=f'Subject:{subject}\n\nBody:{body}'
+    eml.sendmail(EMAIL_ADDRESS,'anikdavis1999@gmail.com',msg)
+
+import smtplib,ssl
+context = ssl.create_default_context()
+
+conn = smtplib.SMTP_SSL('imap.gmail.com',587,context)
+conn.ehlo()
+conn.starttls()
+conn.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+conn.sendmail(EMAIL_ADDRESS,EMAIL_ADDRESS,'Subject: What you like? \n\n Reply Reply Reply')
+conn.quit()
+'''
+
+# import smtplib
+# from email.utils import formataddr
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+
+
+# msg = MIMEMultipart()
+# body_part = MIMEText('Write you text here.')
+# user = EMAIL_ADDRESS
+# password = EMAIL_PASSWORD
+# msg['Subject'] = 'anik'
+# msg['From'] = user
+# msg['To'] = 'anikdavis18@gmail.com'
+# msg.attach(body_part)
+# smtp_obj = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+# smtp_obj.login(user, password)
+# smtp_obj.sendmail(msg['From'], msg['To'], msg.as_string())
+# smtp_obj.quit()
 import os
 from email.message import EmailMessage
 import ssl
